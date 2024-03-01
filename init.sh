194768879cff11d4b260ea85f9291d7abdaaabed
willgladstone <will.gl@dst.one>
2024-03-01 15:19:47 -0500

    schema integration scripts

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+
+$plugin_path = (Get-Location).Path
+
+Write-Output "## Wealth Management Schema Plugin Config Script ##"
+Write-Output "$($plugin_path)"
+
+$config = Get-Content -Raw -Path "$($plugin_path)/config.json" | ConvertFrom-Json
+
+$schemas = Get-ChildItem "$($plugin_path)/config/schemas/*.json"
+
+foreach ($schema in $schemas) {
+    $schemaObject = Get-Content -Raw -Path "$($schema)" | ConvertFrom-Json
+    if ($null -ne $schemaObject) {
+        $schemaJSON = ConvertTo-Json -depth 10 -Compress -InputObject $schemaObject
+        $split_name = ($schema.BaseName).split("-",2)
+        $priority = $split_name[0]
+        $token = $split_name[1]
+
+        & "/fieldsets-bin/import-json.sh" -token "$($token)" -source 'crm' -json "$($schemaJSON)" -type 'schema' -priority $($priority)
+    }
+}
