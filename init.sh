99cc626fa7adb2a467345a2b5726190385379721
willgladstone <will.gl@dst.one>
2024-03-20 17:27:38 -0400

    change parents

--- init.sh
+++ init.sh
@@ -9,6 +9,8 @@ $config = Get-Content -Raw -Path "$($plugin_path)/config.json" | ConvertFrom-Jso
 
 $schemas = Get-ChildItem "$($plugin_path)/config/schemas/*.json"
 
+$data = Get-ChildItem "$($plugin_path)/data/*.json"
+
 foreach ($schema in $schemas) {
     $schemaObject = Get-Content -Raw -Path "$($schema)" | ConvertFrom-Json -NoEnumerate -AsHashtable
     if ($null -ne $schemaObject) {
@@ -18,5 +20,17 @@ foreach ($schema in $schemas) {
         $token = $split_name[1]
 
         & "/fieldsets-bin/import-json.sh" -token "$($token)" -source 'crm' -json "$($schemaJSON)" -type 'schema' -priority $($priority)
+    }
+}
+
+foreach ($datapoint in $data) {
+    $dataObject = Get-Content -Raw -Path "$($datapoint)" | ConvertFrom-Json -NoEnumerate -AsHashtable
+    if ($null -ne $dataObject) {
+        $dataJSON = ConvertTo-Json -depth 10 -Compress -InputObject $dataObject
+        $split_name = ($datapoint.BaseName).split("-",2)
+        $priority = $split_name[0]
+        $token = $split_name[1]
+
+        & "/fieldsets-bin/import-json.sh" -token "$($token)" -source 'wealth-management' -json "$($dataJSON)" -type 'data' -priority $($priority)
     }
 }
